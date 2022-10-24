e2d9d6a6e3a70505e63b898ac3793ab709bbddee
HandyGold75 <97157359+HandyGold75@users.noreply.github.com>
2022-10-24 22:15:31 +0200

    24-10-2022

--- Client.js
+++ Client.js
@@ -1,44 +1,20 @@
-function LOGIN() {
-    obj.ws.send("<LOGIN_TOKEN> " + obj.AUTHTOKEN)
-};
-function LOGIN_TOKEN_FAIL() {
-    obj.AUTHTOKEN = prompt("AUTH Token");
-    LOGIN()
-};
-function LOGOUT() {
-    obj.ws.close()
-};
-function SHUTDOWN() {
-    obj.ws.close()
-};
-
-
 class obj {
     ws;
     IP;
     PORT;
-    AUTHTOKEN;
     lastCom;
     lastMsg1;
     lastMsg2;
-    fmap;
 }
 
 function wsStart() {
     if (obj.ws === undefined) {
         obj.ws = null;
         obj.IP = "127.0.0.1";
-        obj.PORT = 6900;
-        obj.AUTHTOKEN = "89UibZOFCKmPObSBnBxSNcorbp4eUYAKPX5V5qepEYw7tVwO0nZ3wwXGK48VXBjc";
+        obj.PORT = 7005;
         obj.lastCom = "";
         obj.lastMsg1 = "";
         obj.lastMsg2 = "";
-        obj.fmap = {
-            "<LOGIN>": LOGIN,
-            "<LOGIN_TOKEN_FAIL>": LOGIN_TOKEN_FAIL,
-            "<LOGOUT>": LOGOUT,
-            "<SHUTDOWN>": SHUTDOWN
-        };
 
         obj.ws = new WebSocket("ws://" + obj.IP + ":" + obj.PORT);
 
@@ -48,17 +24,14 @@ function wsStart() {
 
         obj.ws.onmessage = ({ data }) => {
             obj.lastMsg1 = data;
-
-            if (data in obj.fmap) {
-                obj.fmap[data]()
-            }
+            console.log("Got message: " + data)
         };
     }
 };
 
 
 
-function wsUpState() {
+function wsUpState(){
     if (obj.ws.readyState === 0 || obj.ws.readyState === 1) {
         return true
     }
